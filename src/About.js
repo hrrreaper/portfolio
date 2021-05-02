1d7e7125b3c005c6d4b6b59b2a116251b3c40ac4
Heather Reaper <heather.reaper@gmail.com>
2021-05-02 11:33:51 -0400

    fix to repo

--- src/About.js
+++ src/About.js
@@ -1,18 +1,32 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Element } from 'react-scroll';
+import pic1 from './assets/pic1.jpg';
 
 const About = () => {
   return (
     <Element id='about' name='about' className="element">
       <Wrapper>
-        <div>
-        I'm Heather Reaper blah blah
-        </div>
+        <Img src={pic1} />
+        <Text>
+          <p>
+            Hi, I'm Heather! A recent graduate from Concordia University's Full-Stack Web Development program. I have over 5 year's experience in digital marketing where I worked closely with developers and found their work more interesting than mine.
+          </p>
+        </Text>
         </Wrapper>
     </Element>
   )
 }
+
+const Img = styled.img`
+  width: 250px;
+  border-radius: 50%;
+  margin-right: 20px;
+`;
+
+const Text = styled.div`
+  width: 40vw;
+`;
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -20,7 +34,7 @@ const Wrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  flex-direction: column;
+  flex-direction: row;
 `;
 
 export default About;
